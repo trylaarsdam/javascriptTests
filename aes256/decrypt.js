28f3a7ec6a07495ee05f8cade0e6ff8b9b937df1
Todd Rylaarsdam <todd@toddr.org>
2021-09-28 15:44:45 -0500

    base64 decode

--- aes256/decrypt.js
+++ aes256/decrypt.js
@@ -1,10 +1,10 @@
 const CryptoJS = require('crypto-js');
 
-var ciphertext = "ee56cd433172d5ec433f3d201f39a9dc701ff17d10c09999b99f5045f6430e7b";
+var ciphertext = "7lbNQzFy1exDPz0gHzmp3FZXMYcrjZKtkkkLsKXCmbQ=";
 var key = "11111111111111111111111111111111";
 var iv = "";
 
-var ciphertextWA = CryptoJS.enc.Hex.parse(ciphertext);
+var ciphertextWA = CryptoJS.enc.Base64.parse(ciphertext);
 var keyWA = CryptoJS.enc.Utf8.parse(key);
 var ivWA = CryptoJS.enc.Utf8.parse(iv);
 var ciphertextCP = { ciphertext: ciphertextWA };
