696192efa115ec2bd1cf26a0950c62bf414fd742
Todd Rylaarsdam <todd@toddr.org>
2021-09-28 15:42:32 -0500

    working decrypt

--- aes256/decrypt.js
+++ aes256/decrypt.js
@@ -2,7 +2,7 @@ const CryptoJS = require('crypto-js');
 
 var ciphertext = "ee56cd433172d5ec433f3d201f39a9dc701ff17d10c09999b99f5045f6430e7b";
 var key = "11111111111111111111111111111111";
-var iv = "0000000000000000";
+var iv = "";
 
 var ciphertextWA = CryptoJS.enc.Hex.parse(ciphertext);
 var keyWA = CryptoJS.enc.Utf8.parse(key);
@@ -12,7 +12,10 @@ var ciphertextCP = { ciphertext: ciphertextWA };
 var decrypted = CryptoJS.AES.decrypt(
     ciphertextCP,
     keyWA, 
-    { iv: ivWA }
+    { iv: ivWA,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Iso10126 
+    }
 );
 
 console.log(decrypted.toString(CryptoJS.enc.Utf8)); // Apple
