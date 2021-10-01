392b27033db10879adeb8f5e1f7c9ebc294599ed
Todd Rylaarsdam <todd@toddr.org>
2021-10-01 15:55:34 -0500

    updated

--- date/date.js
+++ date/date.js
@@ -1,7 +1,8 @@
+var stdio = require('stdio');
 var ops = stdio.getopt({
 	'start': {args:1, required: true},
 	'end': {args:1, required: true}
 });
 
-var date = (new Date(start)).getDate()
+var date = (new Date(parseInt(ops.start))).toISOString()
 console.log(date);
