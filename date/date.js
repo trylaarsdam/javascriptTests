06cde75330a88a590e5f89ebeb2426b0edaacbd0
Todd Rylaarsdam <todd@toddr.org>
2021-10-06 09:36:40 -0500

    parameter test

--- date/date.js
+++ date/date.js
@@ -1,8 +1,10 @@
+var options = {year: 'numeric', month: 'numeric', day: 'numeric'}
+
 var stdio = require('stdio');
 var ops = stdio.getopt({
 	'start': {args:1, required: true},
 	'end': {args:1, required: true}
 });
 
-var date = (new Date(parseInt(ops.start))).toISOString()
+var date = (new Date(parseInt(ops.start))).toLocaleDateString('en-EN', options).replaceAll('/','-')
 console.log(date);
---
+++ parameters/index.js
@@ -0,0 +1,3 @@
+console.log(process.argv.slice(2)[0]);
+console.log(process.argv.slice(2)[1]);
+console.log(process.argv.slice(2)[2]);
