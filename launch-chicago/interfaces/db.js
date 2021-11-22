76dc129a7b8d086709c1d70524474687ccf07541
Todd Rylaarsdam <todd@toddr.org>
2021-11-21 22:06:46 -0600

    updates

---
+++ launch-chicago/interfaces/db.js
@@ -0,0 +1,10 @@
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+const serviceAccount = require('./path/to/serviceAccountKey.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
